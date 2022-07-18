999020f14a31ec53eb1b1168c37575e8f8855e8b
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-19 02:04:10 +0530

    contours created

--- scroll.py
+++ scroll.py
@@ -7,6 +7,8 @@ while True:
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_black, upper_black)
+    contours, hierachy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0,255,0), 2)
 
 
     cv2.imshow('Frame', frame)
