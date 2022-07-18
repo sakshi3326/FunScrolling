afcc7587c5c08f6b4515e67783f3c83242bbe537
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-19 02:08:06 +0530

    printing area

--- scroll.py
+++ scroll.py
@@ -7,8 +7,15 @@ while True:
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_black, upper_black)
+
     contours, hierachy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 300:
+
+            print(area)
     cv2.drawContours(frame, contours, -1, (0,255,0), 2)
+
 
 
     cv2.imshow('Frame', frame)
