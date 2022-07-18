04b56be9f331121583d41594536d55f35a3b59ff
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-19 02:33:21 +0530

    scrolled and finished

--- scroll.py
+++ scroll.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
+import pyautogui
 cap = cv2.VideoCapture(0)
-lower_black = np.array([0, 0, 0])
-upper_black = np.array([350,55,100])
+lower_red = np.array([0,50,50])
+upper_red = np.array([10,255,255])
+
+prev_y = 0
+
 while True:
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_black, upper_black)
+    mask = cv2.inRange(hsv, lower_red, upper_red)
 
     contours, hierachy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
@@ -15,11 +19,12 @@ while True:
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
-
-
+            if y < prev_y:
+                pyautogui.press('space')
+            prev_y = y
 
     cv2.imshow('Frame', frame)
-    cv2.imshow('mask', mask)
+
 
     if cv2.waitKey(10) == ord('q'):
         break
